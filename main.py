9ed208433474f9879db249b92f4f479948f4629a
alsbringer <mkhadafi0001@gmail.com>
2026-09-09 16:41:06 +0700

    refactor: improve varaible name, and reduce magic number

--- main.py
+++ main.py
@@ -16,12 +16,14 @@ class Platforms(Entity):
     def __init__(self, left, top, img):
         super().__init__(left, top, img)
                 
-class Jane(Entity):
+class Player(Entity):
     def __init__(self, left, top, img):
         super().__init__(left, top, img)
         
         # direction state
         self.move_speed = 10
+        self.jump_speed = 10
+        self.fall_speed = 20
         self.face_direction = "right"
         self.forward_blocked = False
         self.backward_blocked = False
@@ -33,8 +35,9 @@ class Jane(Entity):
         self.jump_height = 100
         self.extra_jump_height = 0
         # ground state
-        self.jane_ground = self.bottom
+        self.player_ground = self.bottom
         self.on_platform = False
+        self.starting_ground = self.bottom
 
 class Cheese(Entity):
     def __init__(self, left, top, img):
@@ -72,8 +75,8 @@ def flip_y(image):
 def flip_xy(image):
     return pygame.transform.flip(image, True, True)
 
-def fade_on_approach(jane_x, target_x, target_img):
-    distance = target_x - jane_x
+def fade_on_approach(player_x, target_x, target_img):
+    distance = target_x - player_x
     if distance <= 100:
         target_img.set_alpha(int(255 * distance / 100))
     else:
@@ -83,16 +86,27 @@ def fade_on_approach(jane_x, target_x, target_img):
 # --- Environtment Setup ---
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 500
+BACKGROUND_IMG = loadImage(join("assets","amphoreus.png"))
+BACKGROUND_IMG = setSize_W(BACKGROUND_IMG, 1000)
+WIDTH_MARGIN = 20
+HEIGHT_MARGIN = 34
+
 pygame.init()
 display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 
-# --- Jane Setup ---
-jane_initial_img = loadImage(join("assets","jane1.png")).convert_alpha()
-jane_initial_img = setSize_W(jane_initial_img, 100)
-jane_initial_top = WINDOW_HEIGHT-jane_initial_img.get_height()
-jane_initial_left = 0
-jane = Jane( top= jane_initial_top ,left = jane_initial_left , img=jane_initial_img)
+# --- Avatar Setup ---
+aglaea_initial_img = loadImage(join("assets","aglaea","agy-1.jpe")).convert_alpha()
+aglaea_initial_img = setSize_W(aglaea_initial_img, 100)
+aglaea_initial_top = WINDOW_HEIGHT-aglaea_initial_img.get_height()
+aglaea_initial_left = 0
+aglaea = Player( top= aglaea_initial_top ,left = aglaea_initial_left , img=aglaea_initial_img)
+
+caelus_initial_img = loadImage(join("assets","caelus","caelus-2.jpe")).convert_alpha()
+caelus_initial_img = setSize_W(caelus_initial_img, 100)
+caelus_initial_top = WINDOW_HEIGHT-caelus_initial_img.get_height()
+caelus_initial_left = 0
+caelus = Player( top= caelus_initial_top ,left = caelus_initial_left , img=caelus_initial_img)
 
 # --- Cheese Setup ---
 cheese_initial_img = loadImage(join("assets","cheese.png")).convert_alpha()
@@ -109,13 +123,10 @@ firewall_initial_top = int(calc_align_bottom(firewall_initial_img) + firewall_in
 firewall = Platforms(left= firewall_initial_left, top= firewall_initial_top, img=firewall_initial_img)
 
 
-# --- Movement ---
-starting_ground = WINDOW_HEIGHT
-
 # --- Main Loop ---
 running = True
 while running:
-    prev_ground = jane.jane_ground
+    prev_ground = aglaea.player_ground
     keys = pygame.key.get_pressed()
 
 #SECTION: STATE_&_INPUT_MANAGEMENT
@@ -127,136 +138,142 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                print("can_jump:", jane.can_jump)
-                if jane.can_double_jump and not jane.can_jump:
-                    print("second jump at", jane.bottom)
-                    jane.jumping_direction = "up"
-                    jane.extra_jump_height += 80
-                    jane.can_double_jump = False
-                elif jane.can_jump:
-                    jane.jumping = True
-                    jane.jumping_direction = "up"
-                    jane.can_jump = False
-                    jane.can_double_jump = True
-                    print("Jumping:", jane.jumping, "at", jane.bottom)
-                    starting_ground = jane.jane_ground
+                print("can_jump:", aglaea.can_jump)
+                if aglaea.can_double_jump and not aglaea.can_jump:
+                    print("second jump at", aglaea.bottom)
+                    aglaea.jumping_direction = "up"
+                    aglaea.extra_jump_height += 80
+                    aglaea.can_double_jump = False
+                elif aglaea.can_jump:
+                    aglaea.jumping = True
+                    aglaea.jumping_direction = "up"
+                    aglaea.can_jump = False
+                    aglaea.can_double_jump = True
+                    print("Jumping:", aglaea.jumping, "at", aglaea.bottom)
+                    aglaea.starting_ground = aglaea.player_ground
             if event.key == pygame.K_f:
                 if (
-                    jane.right >= cheese.left and jane.right <= cheese.right
-                    or jane.left >= cheese.left and jane.left <= cheese.right 
+                    aglaea.right >= cheese.left and aglaea.right <= cheese.right
+                    or aglaea.left >= cheese.left and aglaea.left <= cheese.right 
                     ):
                     if cheese.taken: cheese.taken = False
                     else: cheese.taken = True
     # Logic(move foreward, move backward, flip direction)
     # Input(D, A)
     if keys[pygame.K_d]:
-        if not jane.forward_blocked:
-            jane.left += jane.move_speed
-            if jane.face_direction != "right":
-                jane.face_direction = "right"
-                jane.img = flip_x(jane.img)
+        if not aglaea.forward_blocked:
+            aglaea.left += aglaea.move_speed
+            if aglaea.face_direction != "right":
+                aglaea.face_direction = "right"
+                aglaea.img = flip_x(aglaea.img)
         # else: print("forward Blocked"
-    if keys[pygame.K_a] and not jane.backward_blocked:
-        jane.left -= jane.move_speed
-        if jane.face_direction != "left":
-            jane.face_direction = "left"
-            jane.img = flip_x(jane.img)
+    if keys[pygame.K_a] and not aglaea.backward_blocked:
+        aglaea.left -= aglaea.move_speed
+        if aglaea.face_direction != "left":
+            aglaea.face_direction = "left"
+            aglaea.img = flip_x(aglaea.img)
+    
     # Logic(collision blocking)
     if (
-        (jane.right >= firewall.left + int(firewall.width / 2) - 20)   
+        (aglaea.right >= firewall.left + int(firewall.width / 2) - WIDTH_MARGIN)   
         and 
-        (jane.right <= firewall.left + int(firewall.width / 2) + 20)
+        (aglaea.right <= firewall.left + int(firewall.width / 2) + WIDTH_MARGIN)
         and 
-        (jane.bottom > firewall.top + 34)
+        (aglaea.bottom > firewall.top + HEIGHT_MARGIN)
         ):
-        jane.forward_blocked = True
+        aglaea.forward_blocked = True
     else:
-        jane.forward_blocked = False    
+        aglaea.forward_blocked = False    
     if (
-        (jane.left >= firewall.left + int(firewall.width / 2) - 20 )
+        (aglaea.left >= firewall.left + int(firewall.width / 2) - WIDTH_MARGIN )
         and 
-        (jane.left <= firewall.left + int(firewall.width / 2) + 20)
+        (aglaea.left <= firewall.left + int(firewall.width / 2) + WIDTH_MARGIN)
         and
-        (jane.bottom > firewall.top + 34)
+        (aglaea.bottom > firewall.top + HEIGHT_MARGIN)
         ):
-        jane.backward_blocked = True
+        aglaea.backward_blocked = True
     else:
-        jane.backward_blocked = False
+        aglaea.backward_blocked = False
 
     # Logic(ground to firewall)
-    if (jane.bottom <= firewall.top + 34
-        and jane.right >= firewall.left
-        and jane.left <= firewall.right):
-        jane.on_platform = True
-        jane.jane_ground = firewall.top + 34
+    if (aglaea.bottom <= firewall.top + HEIGHT_MARGIN
+        and aglaea.right >= firewall.left
+        and aglaea.left <= firewall.right):
+        aglaea.on_platform = True
+        aglaea.player_ground = firewall.top + HEIGHT_MARGIN
     else:
-        jane.on_platform = False
+        aglaea.on_platform = False
     # Logic(firewall to ground)
-    if not jane.on_platform:
-        jane.jane_ground = WINDOW_HEIGHT
-    if jane.jane_ground != prev_ground:
+    if not aglaea.on_platform:
+        aglaea.player_ground = WINDOW_HEIGHT
+    if aglaea.player_ground != prev_ground:
         print("ground berubah")
-        print("on platform", jane.on_platform)
-        if not jane.on_platform and not jane.jumping:
+        print("on platform", aglaea.on_platform)
+        if not aglaea.on_platform and not aglaea.jumping:
             print("guing_up false")
-            jane.jumping = True
-            jane.jumping_direction = "down"
+            aglaea.jumping = True
+            aglaea.jumping_direction = "down"
     # Log(ground changing)
-    if jane.jane_ground != prev_ground:
+    if aglaea.player_ground != prev_ground:
         print("prev ground: ", prev_ground)
-        print("current ground ", jane.jane_ground)
+        print("current ground ", aglaea.player_ground)
+
     # Logic(update postition)
     # Log(foot coordinate)
-    jane.right = jane.left + jane.width
-    if jane.jumping:
+    aglaea.right = aglaea.left + aglaea.width
+    if aglaea.jumping:
         # start jumping
-        if jane.jumping_direction == "up":
-            jane.bottom -= 10
-            jane.top = jane.bottom - jane.height
-            print("going up: ", jane.bottom)
+        if aglaea.jumping_direction == "up":
+            aglaea.bottom -= aglaea.jump_speed
+            aglaea.top = aglaea.bottom - aglaea.height
+            print("going up: ", aglaea.bottom)
         # reach maxiimun
-        elif jane.jumping_direction == "down" and jane.bottom < jane.jane_ground:
-            print("going down: ", jane.bottom)
-            jane.bottom += 10
-            jane.top = jane.bottom - jane.height
+        elif aglaea.jumping_direction == "down" and aglaea.bottom < aglaea.player_ground:
+            print("going down: ", aglaea.bottom)
+            aglaea.bottom += aglaea.fall_speed
+            aglaea.top = aglaea.bottom - aglaea.height
         # reach the ground === makes stay
-        if jane.bottom >= jane.jane_ground:
-            jane.jumping = False
-            jane.can_jump = True
-            print("on the ground: ", jane.bottom)
-            jane.can_double_jump = False
-            jane.extra_jump_height = 0
+        if aglaea.bottom >= aglaea.player_ground:
+            aglaea.jumping = False
+            aglaea.can_jump = True
+            print("on the ground: ", aglaea.bottom)
+            aglaea.can_double_jump = False
+            aglaea.extra_jump_height = 0
         #fall switch === makes fall permission
-        if starting_ground - jane.bottom >= (jane.jump_height + jane.extra_jump_height) and jane.jumping_direction == "up":
-            print("fall at", jane.bottom)
-            print("jump height:", jane.jump_height+ jane.extra_jump_height)
-            jane.jumping_direction = "down"
+        if aglaea.starting_ground - aglaea.bottom >= (aglaea.jump_height + aglaea.extra_jump_height) and aglaea.jumping_direction == "up":
+            print("fall at", aglaea.bottom)
+            print("jump height:", aglaea.jump_height+ aglaea.extra_jump_height)
+            aglaea.jumping_direction = "down"
             
 # SECTION(RENDER)
-    display.fill("black")
-    # firewall & jane render
-    if jane.left <= (firewall.right/ 2):  
+    display.blit(BACKGROUND_IMG, (0,0))
+    display.blit(caelus.img, (caelus.left,caelus.top))
+    # firewall & avatar render
+    if aglaea.right <= (firewall.right - firewall.width/2): 
+        print("aglaea on top")
         sprites = [
             (firewall.img, firewall.left, firewall.top),
-            (jane.img, jane.left, jane.top),
+            (aglaea.img, aglaea.left, aglaea.top),
         ] 
     else:
+        print("aglaea on the bottom")
         sprites = [
-            (jane.img, jane.left, jane.top),
+            (aglaea.img, aglaea.left, aglaea.top),
             (firewall.img, firewall.left, firewall.top),
         ]
-    for sprite, sx, sy in sprites:
-        display.blit(sprite, (sx, sy))
+        print(sprites)
+    for sprite_img, sprite_x, sprite_y in sprites:
+        display.blit(sprite_img, (sprite_x, sprite_y))
     
     # cheese render
     if cheese.taken:
-        if jane.face_direction =="right":
-            cheese.left = jane.right - 40
-            cheese.top = jane.bottom - 40
+        if aglaea.face_direction =="right":
+            cheese.left = aglaea.right - 40
+            cheese.top = aglaea.bottom - 40
             display.blit(cheese.img, (cheese.left, cheese.top))
-        if jane.face_direction == "left":
-            cheese.left = jane.left
-            cheese.top = jane.bottom - 40
+        if aglaea.face_direction == "left":
+            cheese.left = aglaea.left
+            cheese.top = aglaea.bottom - 40
             display.blit(cheese.img, (cheese.left, cheese.top))
     else:
         cheese.top = calc_align_bottom(cheese.img)
